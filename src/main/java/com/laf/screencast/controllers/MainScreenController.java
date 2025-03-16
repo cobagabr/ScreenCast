4cdbc1fc5513f4fbc5b6cb3e46f82cb978a28b7b
Gabe Cobabe <gcobab1@wgu.edu>
2025-03-15 21:24:53 -0600

    Reverting branch, going to make a fresh start in an attempt to use websockets instead of a rest API

--- src/main/java/com/laf/screencast/controllers/MainScreenController.java
+++ src/main/java/com/laf/screencast/controllers/MainScreenController.java
@@ -1,5 +1,6 @@
 package com.laf.screencast.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,14 +21,24 @@ public class MainScreenController {
         BufferedImage image = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
     }
 
-    @GetMapping("/mainscreen")
-    public String mainScreen(ModelMap model) throws IOException, NullPointerException {
+    @GetMapping(value ="/mainscreen",produces = "multipart/x-mixed-replace;boundary=frame")
+    public String mainScreen(HttpServletResponse response, OutputStream outputStream) throws IOException, NullPointerException, InterruptedException {
+        response.setContentType("multipart/x-mixed-replace;boundary=frame");
+        OutputStream out = response.getOutputStream();
+        while(true){
         BufferedImage image = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "png", baos);
-        InputStream is = new ByteArrayInputStream(baos.toByteArray());
-        String pic ="data:image/png;base64," + Base64.getEncoder().encodeToString(baos.toByteArray());
-        model.addAttribute("picture",pic);
-        return "mainscreen";
+//        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+//        String pic ="data:image/png;base64," + Base64.getEncoder().encodeToString(baos.toByteArray());
+            byte[] imageBytes = baos.toByteArray();
+            out.write("--frame\r\nContent-Type: image/jpeg\r\n\r\n".getBytes());
+            out.write(imageBytes);
+            out.write("\r\n".getBytes());
+            out.flush();
+            Thread.sleep(200);
+//        model.addAttribute("picture",pic);
+//        return "mainscreen";
+        }
     }
 }
